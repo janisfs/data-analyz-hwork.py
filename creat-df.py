7181fc72f1c44491d24246ee2946c6530aef5b9c
janisfs <janisfs@mail.ru>
2024-08-15 17:08:44 +0300

    Update creat-df.py

--- creat-df.py
+++ creat-df.py
@@ -1,10 +1,17 @@
 import pandas as pd
 
+# вывести первые 5 строк
 df = pd.read_csv('Cost_of_Living_Index_by_Country_2024.csv')
 print(df.head())
 
+# вывести последние 5 строк
+df = pd.read_csv('Cost_of_Living_Index_by_Country_2024.csv')
+print(df.tail())
+
+# вывести информацию о датафрейме
 df = pd.read_csv('Cost_of_Living_Index_by_Country_2024.csv')
 print(df.info())
 
+# вывести описательные статистики
 df = pd.read_csv('Cost_of_Living_Index_by_Country_2024.csv')
 print(df.describe())
