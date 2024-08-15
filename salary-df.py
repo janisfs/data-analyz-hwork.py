e8a3921dc32f253a911bc20128f83e9f4e08e69b
janisfs <janisfs@mail.ru>
2024-08-15 12:49:07 +0300

    Update salary-df.py

--- salary-df.py
+++ salary-df.py
@@ -1,1 +1,17 @@
 
+import pandas as pd
+
+df = pd.read_csv('dz.csv')
+# print(df.head())
+
+# df = pd.read_csv('dz.csv')
+# print(df.info())
+
+# df = pd.read_csv('dz.csv')
+# print(df.describe())
+
+df.fillna(0, inplace=True)  # Заменить все NaN на 0
+print(df)
+
+group = df.groupby('City')['Salary']. mean()
+print(group)
