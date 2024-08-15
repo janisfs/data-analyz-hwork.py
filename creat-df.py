7ada42f7cd16350665183fa2f7d1c8c5655f4274
janisfs <janisfs@mail.ru>
2024-08-15 12:35:22 +0300

    add new code

---
+++ creat-df.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.read_csv('Cost_of_Living_Index_by_Country_2024.csv')
+print(df.head())
+
+df = pd.read_csv('Cost_of_Living_Index_by_Country_2024.csv')
+print(df.info())
+
+df = pd.read_csv('Cost_of_Living_Index_by_Country_2024.csv')
+print(df.describe())
